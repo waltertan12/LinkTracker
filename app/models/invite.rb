517b52def831fcc00540ddb786e6b793ad4b3c13
Walter Tan <wtan@coursehero.com>
2016-12-22 00:16:58 -0800

    Add model level logic for Invites and Users

--- app/models/invite.rb
+++ app/models/invite.rb
@@ -4,7 +4,7 @@
 #
 #  id              :integer          not null, primary key
 #  invite_digest   :string           not null
-#  valid           :boolean          default("false")
+#  is_valid        :boolean          default("false")
 #  expiration_date :datetime         not null
 #  created_at      :datetime         not null
 #  updated_at      :datetime         not null
@@ -12,6 +12,26 @@
 
 class Invite < ApplicationRecord
   validates :invite_digest, presence: true
-  validates :valid, inclusion: { in: [true, false] }
+  validates :is_valid, inclusion: { in: [true, false] }
   validates :expiration_date, presence: true
+
+  def self.generate_invite
+    Invite.create(
+      invite_digest: SecureRandom::base64(32), 
+      is_valid: true, 
+      expiration_date: 1.day.from_now
+    )
+  end
+
+  def self.redeem_invite(invite)
+    invite = Invite.find_by(invite_digest: invite.invite_digest, is_valid: true)
+
+    invite.destroy if invite && invite.expiration_date > Time.now
+  end
+
+  def self.destroy_invalid_invites
+    Invite.where(is_valid: false)
+      .or(Invite.where("expiration_date < ?", Time.now))
+      .destroy_all
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -16,6 +16,8 @@ class User < ApplicationRecord
 
   EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 
+  after_initialize :ensure_session_token
+
   validates :email, presence: true, length: { maximum: 255 }, 
     format: { with: EMAIL_REGEX },
     uniqueness: { case_sensitive: false }
