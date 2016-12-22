ca140ded20fd3ff9c79fec10b9e05c5d1b2f8dcc
Walter Tan <wtan@coursehero.com>
2016-12-22 00:17:46 -0800

    Update src files

--- src/app.jsx
+++ src/app.jsx
@@ -1,11 +1,11 @@
 import styles from './stylesheets/index.scss';
-import React, { Component, PropTypes } from 'react';
+import React, { Component } from 'react';
 
 class App extends Component {
     render() {
         return (
             <div className={styles.container}>
-                Hello
+                Hello, Jafart!
             </div>
         );
     }
--- src/entry.jsx
+++ src/entry.jsx
@@ -1,6 +1,5 @@
 import App from './app.jsx';
+import React from 'react';
 import ReactDOM from 'react-dom';
 
-const store = configureStore();
-
-ReactDOM.render(App, document.getElementById('app'));
+ReactDOM.render(<App />, document.getElementById('app'));
