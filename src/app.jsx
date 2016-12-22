0e5e031326a57b2daddc8f79d9de5333ff666704
Walter Tan <wtan@coursehero.com>
2016-12-22 10:07:12 -0800

    Build mock-up

--- src/app.jsx
+++ src/app.jsx
@@ -3,9 +3,49 @@ import React, { Component } from 'react';
 
 class App extends Component {
     render() {
+        let divStyle = {
+            display: 'flex',
+            justifyContent: 'space-between',
+            marginBottom: '15px'
+        };
+
+        let otherDivStyle = {
+            display: 'flex',
+            flexDirection: 'column'
+        }
+
+        let searchOptions = [ 'all', 'tag', 'name', 'url', 'date' ];
         return (
             <div className={styles.container}>
-                Hello, Jafart!
+                <h1>Pouch</h1>
+                <form className={styles.formGroup}>
+                    <div style={otherDivStyle}>
+                        <div style={divStyle}>
+                            <input 
+                                type="text" 
+                                placeholder="Search for something" 
+                                className={styles.formControl}/>
+                            <select className={styles.formControl} style={{width:'25%'}}>
+                                {searchOptions.map(option => {
+                                    return <option value={option} key={option}>
+                                        {option[0].toUpperCase() + option.slice(1)}
+                                    </option>
+                                })}
+                            </select>
+                            <select className={styles.formControl} style={{width:'25%'}}>
+                                <option value="standard">Standard</option>
+                                <option value="regex">Regex</option>
+                            </select>
+                            <div className={styles.btn + ' ' + styles.btnSuccess}>+</div>
+                            <div className={styles.btn + ' ' + styles.btnDanger}>-</div>
+                        </div>
+                    </div>
+                </form>
+                <h2>Links</h2>
+                <hr/>
+                <ul>
+                    <li>https://google.com</li>
+                </ul>
             </div>
         );
     }
