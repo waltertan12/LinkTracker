aa67d5a171a00b8bc3813cd4c9cf231f87586c08
Walter Tan <wtan@coursehero.com>
2016-12-22 00:15:12 -0800

    Create API routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: "static_pages#index"
+
   namespace :api, defaults: {format: :json} do
-    namespace :v1 do
-      
+    namespace :v1, defaults: {format: :json} do
+      resources :sessions, only: [         :create, :destroy ]
+      resources :invites,  only: [ :index, :create, :destroy ]
+
+      resources :users,    except: [ :new, :edit ]
     end
   end
 end
